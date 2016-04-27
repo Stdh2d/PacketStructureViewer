4b44781768756e072c1b3de245c1847e87b33f36
stdh2d <vincent.studer@lecortex.com>
2016-04-27 13:56:13 +0200

    Beginning of a beautifull tool

---
+++ HexProcessor/HexChunk.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace HexProcessor
+{
+    class HexChunk
+    {
+        private CastType castType;
+        private string hexString;
+
+        public int Size
+        {
+            get
+            {
+                return ByteSizeOfType(castType);
+            }
+        }
+
+        static int ByteSizeOfType(CastType type)
+        {
+            switch(type)
+            {
+                case CastType.INT16:
+                    return 2;
+                case CastType.INT32:
+                    return 4;
+                case CastType.INT64:
+                default:
+                    return 8;
+            }
+        }
+
+        static string SwapByteEndianness(string hexChain)
+        {
+            string s = "";
+            s += hexChain[2];
+            s += hexChain[3];
+            s += hexChain[0];
+            s += hexChain[1];
+            return s;
+        }
+
+        public void processChunk(string hexChain, Endianness endianness)
+        {
+            if(endianness == Endianness.LITTLE)
+                hexString = SwapByteEndianness(hexChain);
+            else
+                hexString = hexChain;
+        }
+
+        public void SetType(string type)
+        {
+            switch(type)
+            {
+                case "int16":
+                    castType = CastType.INT16;
+                    break;
+                case "int32":
+                    castType = CastType.INT32;
+                    break;
+                case "int64":
+                    castType = CastType.INT64;
+                    break;
+            }
+        }
+
+        public int getInt()
+        {
+            try
+            {
+                return Int16.Parse(hexString, NumberStyles.HexNumber);
+            }
+            catch
+            {
+                return -1;
+            }
+        }
+
+        public override string ToString()
+        {
+            string res = "";
+            switch(castType)
+            {
+                case CastType.INT16:
+                    res = getInt().ToString();
+                    break;
+            }
+            return res;
+        }
+    }
+}
---
+++ HexProcessor/Processor.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HexProcessor
+{
+    enum CastType
+    {
+        INT16,
+        INT32,
+        INT64
+    }
+
+    enum Endianness
+    {
+        BIG,
+        LITTLE
+    }
+
+    public class Processor
+    {
+        private List<HexChunk> chunkList;
+
+        public Processor()
+        {
+            chunkList = new List<HexChunk>();
+        }
+
+        public void ProcessString(string s, List<string> typeList)
+        {
+            string[] bytes = s.Split(' ');
+
+            foreach(string typeString in typeList)
+            {
+                HexChunk hc = new HexChunk();
+                hc.SetType(typeString);
+                hc.processChunk(String.Concat(bytes.SubArray(0, hc.Size)), Endianness.LITTLE);
+                chunkList.Add(hc);
+            }
+        }
+
+        public string GetResult()
+        {
+            return String.Concat(chunkList);
+        }
+    }
+}
